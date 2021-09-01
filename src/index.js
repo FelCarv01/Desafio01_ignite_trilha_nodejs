262f162f0e4b475b4823cfa70e75f52786961792
Felipe Carvalho <feliped3carvalho@gmail.com>
2021-09-01 14:11:53 -0300

    Criado feature de atualizar todo se feito

--- src/index.js
+++ src/index.js
@@ -78,7 +78,19 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { done } = request.query;
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "TODO not found!" });
+  }
+
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
